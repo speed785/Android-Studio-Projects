0c7de608d77370a0bbc7e5955d9c43f467ecb4d4
James <ioandumitru59@gmail.com>
2019-09-12 15:14:25 -0500

    Added a homescreen icon to the app

--- app/src/main/java/com/example/tempconverter/MainActivity.java
+++ app/src/main/java/com/example/tempconverter/MainActivity.java
@@ -4,9 +4,12 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.Toast;
+import android.graphics.Color;
 import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
+    View view; //create object to manipulate background color
     private EditText text;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,16 +34,29 @@ public class MainActivity extends AppCompatActivity {
                 float inputValue =
                         Float.parseFloat(text.getText().toString());
                 if (celsiusButton.isChecked()) {
-
                     text.setText(String.valueOf(ConverterUtil.convertCelsiusToFahrenheit(inputValue)));
                     celsiusButton.setChecked(false);
                     fahrenheitButton.setChecked(true);
                 }
                 else {
-
                     text.setText(String.valueOf(ConverterUtil.convertFahrenheitToCelsius(inputValue)));
                     fahrenheitButton.setChecked(false);
                     celsiusButton.setChecked(true);
+                }
+                //grab CURRENT result value now in Text Field
+                inputValue = Float.parseFloat(text.getText().toString());
+                view = findViewById(R.id.activity_main);
+                if (inputValue>90)
+                    //set hex color to skyblue
+                    view.setBackgroundColor(Color.parseColor("#87ceff"));
+                    //input value = degrees in fahrenheit
+                else if (inputValue<32)
+                    //set hex color to fuchsia(magenta)
+                    //It's a dangerous red/pink!
+                    view.setBackgroundColor(Color.parseColor("#FF00FF"));
+                else
+                {
+                    view.setBackgroundColor(Color.YELLOW);
                 }
                 break;
         }
