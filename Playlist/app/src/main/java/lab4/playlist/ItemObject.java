e1227b95c7883ac1a66c437e2319a6b6a447fe36
James <ioandumitru59@gmail.com>
2019-10-29 17:32:28 -0500

    Added the getters and setters for the title, year, artist, country, company, sold and price.

--- Playlist/app/src/main/java/lab4/playlist/ItemObject.java
+++ Playlist/app/src/main/java/lab4/playlist/ItemObject.java
@@ -1,4 +1,59 @@
 package lab4.playlist;
 
 public class ItemObject {
+    String Title;
+    String Year;
+    String Artist;
+    String Country;
+    String Company;
+    String Sold;
+    int Price;
+
+    public String getTitle() {
+        return Title;
+    }
+    public void setTitle(String title){
+        this.Title = title;
+    }
+    public String getYear() {
+        return Year;
+    }
+    public void setYear(String year) {
+        this.Year = year;
+    }
+    public String getArtist() {
+        return Artist;
+    }
+    public void setArtist(String artist) {
+        this.Artist = artist;
+    }
+    public String getCountry() {
+        return Country;
+    }
+    public void setCountry(String country) {
+        this.Country = country;
+    }
+    public String getCompany() {
+        return Company;
+    }
+    public void setCompany(String company){
+        this.Company = company;
+    }
+    public String getSold() {
+        return Sold;
+    }
+    public void setSold(String sold){
+        this.Sold = sold;
+    }
+    public int getPrice() {
+        return Price;
+    }
+    public void setPrice(int price) {
+        Price = price;
+    }
+    public ItemObject (String songTitle, String songYear, String songArtist){
+        this.Title = songTitle;
+        this.Year = songYear;
+        this.Artist = songArtist;
+    }
 }
--- Playlist/app/src/main/java/lab4/playlist/MainActivity.java
+++ Playlist/app/src/main/java/lab4/playlist/MainActivity.java
@@ -6,13 +6,18 @@ import android.app.Activity;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.widget.ListView;
-
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class MainActivity extends AppCompatActivity {
 
