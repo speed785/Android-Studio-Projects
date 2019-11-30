a9f4abf77b03d2a6572d4dac187294eee3b2bb0f
James Dumitru <36217171+speed785@users.noreply.github.com>
2019-11-29 22:28:38 -0600

    CRUD methods/operations off the database.

--- BookReviews/app/src/main/java/com/example/bookreviews/MainActivity.java
+++ BookReviews/app/src/main/java/com/example/bookreviews/MainActivity.java
@@ -4,11 +4,33 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SqlHelper db = new SqlHelper(this);
+        /** CRUD Operations **/
+        // add Books
+        db.addBook(new Book("Android Studio Development Essentials", "Neil Smyth"));
+        db.addBook(new Book("Beginning Android Application Development", "Wei-Meng Lee"));
+        db.addBook(new Book("Programming Android", "Wallace Jackson"));
+        db.addBook(new Book("Hello, Android", "Wallace Jackson"));
+
+        // get all books
+        List<Book> list = db.getAllBooks();
+
+        // update one book
+        int j = db.updateBook(list.get(0));
+        // delete one book
+        db.deleteBook(list.get(0));
+        // get all books
+        db.getAllBooks();
     }
 }
+
+
